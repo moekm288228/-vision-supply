30168b40e73d85782979195d019791a14e748fb2
moekm288228 <moekm69@gmail.com>
2026-07-22 22:31:50 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ function login() {
   let email = document.querySelector(moekm69@gmail.com).value;
   let password = document.querySelectorAll(moe_km05202008)[1].value;
 
-  if (email === OWNER_EMAIL && password === OWNER_PASSWORD) {
+  if (email === moekm69@gmail.com && password === moe_km05202008) {
     localStorage.setItem("ownerLoggedIn", "true");
     window.location.href = "dashboard.html";
   } else {
