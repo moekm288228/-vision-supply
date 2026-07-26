4730395acb586fb42c204003f3d468f7415fc7b8
moekm288228 <moekm69@gmail.com>
2026-07-25 22:46:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-let products = [
-  {
-    name: "Coming Soon",
-    price: "",
-    status: "Available"
-  }
-];
+let products = JSON.parse(localStorage.getItem("products")) || [];
 
-function loadProducts() {
-  console.log("Vision Supply ready");
+
+function showProducts(){
+
+let area = document.getElementById("products");
+
+if(!area) return;
+
+area.innerHTML = "";
+
+
+products.forEach(product => {
+
+area.innerHTML += `
+
+<div class="card">
+
+<img src="${product.image}" width="200">
+
+<h3>${product.name}</h3>
+
+<p>$${product.price}</p>
+
+<p>${product.status}</p>
+
+</div>
+
+`;
+
+});
+
 }
+
+
+showProducts();
