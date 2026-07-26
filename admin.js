6c7cebb87f212724165b030432d66342c9634304
moekm288228 <moekm69@gmail.com>
2026-07-26 18:14:10 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -16,10 +16,15 @@ quantity: document.getElementById("quantity").value,
 status: document.getElementById("status").value,
 newDrop: document.getElementById("newDrop").value,
 bestSeller: document.getElementById("bestSeller").value
+};
+
 addDoc(collection(db, "products"), product)
 .then(() => {
   alert("Product Added!");
 })
+.catch((error) => {
+  console.log(error);
+});
 .catch((error) => {
   console.log(error);
 });
